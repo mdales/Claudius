11616683d5f8540e5ee496517e8c35c77880e373
Michael Dales <michael@digitalflapjack.com>
2024-01-02 20:05:10 +0000

    Genuary 2 done

--- examples/day2/bin/main.ml
+++ examples/day2/bin/main.ml
@@ -2,14 +2,16 @@ open Claudius
 
 (* ----- *)
 
-let tick (_t : int) (_screen : Tcc.screen) (prev : Framebuffer.t) : Framebuffer.t =
-  let buffer = Array.map (fun row -> 
-    Array.map (fun pixel ->
-      if pixel > 2 then (pixel - 2) else 0
-    ) row
-  ) prev in
-
-  buffer
+let tick (t : int) (screen : Tcc.screen) (_prev : Framebuffer.t) : Framebuffer.t =
+  let palsize = Palette.size screen.palette in
+  Framebuffer.init screen.width screen.height (fun x y -> 
+      let ft = (Float.of_int t) /. 10. and fx = (Float.of_int x) /. 140. and fy = (Float.of_int y) /. 140. in
+      let z = 10.0 +. (sin (ft /. 1000.0) *. 5.0)
+      and d = 10.0 +. (cos (ft /. 1000.0) *. 5.0) in
+      let fc = (sin (sin ((fx +. ft) /. z)) +. sin (sin ((fy +. ft) /. d))) *. Float.of_int(palsize / 2) in
+      let rc = ((int_of_float fc)) mod palsize in
+      if rc >= 0 then rc else (rc + palsize)
+  )
 
 (* ----- *)
 
@@ -18,6 +20,6 @@ let () =
     width = 640 ;
     height = 480 ;
     scale = 1 ;
-    palette = Palette.generate_mono_palette 16 ;
+    palette = Palette.generate_plasma_palette 1024 ;
   } in
   Tcc.tcc_init screen "Genuary Day 2: No Palette" None tick
