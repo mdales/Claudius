62331caa96c131f31384ae4d33e64419597c5bb3
Michael Dales <michael@digitalflapjack.com>
2024-01-06 09:32:19 +0000

    Faux sizecode day 6

--- examples/day6/bin/main.ml
+++ examples/day6/bin/main.ml
@@ -1,47 +1,32 @@
 open Claudius
 
-(* type point = {
-  x : int ;
-  y : int ;
-  vx : int; 
-  xy : int;
-} *)
-
 let tails = 50
-
 
 let triangle (x : float) (amplitude : float) (period : float) : float =
   ((2. *. amplitude) /. Float.pi) *. asin (sin (x *. ((2. *. Float.pi) /. period)))
 
-
 let boot s =
   Framebuffer.init (Screen.dimensions s) (fun _x _y -> 0)
+
+let point d ft = 
+  let fd = Float.of_int d in
+  (d / 2) + Int.of_float (triangle (ft +. Random.float 50.) (fd /. 2.) (50. +. Random.float 100.))
 
 let line t s col = 
   Random.init 789123;
   let ft = Float.of_int t in
   let w, h = Screen.dimensions s in
-  let fh = Float.of_int h 
-  and fw = Float.of_int w in
   Primitives.Line (
-    {
-      x = (w / 2) + Int.of_float (triangle (ft +. Random.float 50.) (fw /. 2.) (50. +. Random.float 100.)) ;
-      y = (h / 2) + Int.of_float (triangle (ft +. Random.float 50.) (fh /. 2.) (50. +. Random.float 100.))
-    }, 
-    {
-      x = (w / 2) + Int.of_float (triangle (ft +. Random.float 50.) (fw /. 2.) (50. +. Random.float 100.)) ;
-      y = (h / 2) + Int.of_float (triangle (ft +. Random.float 50.) (fh /. 2.) (50. +. Random.float 100.))
-    }, 
+    { x = point w ft ; y = point h ft }, 
+    { x = point w ft ; y = point h ft }, 
     col
   )
 
 let tick t s p = 
-  if t >= tails then (
-    let l = line (t - tails) s 0 in
-    Framebuffer.render p [l];
-  );
-  let l = line t s ((t mod ((Palette.size (Screen.palette s)) -1)) + 1) in
-  Framebuffer.render p [l];
+  Framebuffer.render p 
+    (List.concat 
+    [(if t >= tails then [(line (t - tails) s 0)] else []) ; 
+    [line t s ((t mod ((Palette.size (Screen.palette s)) -1)) + 1)]]);
   p
 
 let () =
