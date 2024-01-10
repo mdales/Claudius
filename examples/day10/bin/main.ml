b93ffaf05933c933bc5a2684d8efd6bf39056de4
Michael Dales <michael@digitalflapjack.com>
2024-01-10 08:21:14 +0000

    Something uninspired for day 10

---
+++ examples/day10/bin/main.ml
@@ -0,0 +1,52 @@
+open Claudius
+
+(* ----- *)
+
+let drawpoly (x : int) (y : int) (r : int) (sides : int) (a : float) (col : int) =
+  let segment_angle = (2. *. Float.pi) /. (Float.of_int sides) in
+  let fr = Float.of_int r in
+  let rec loop = (fun (i : int) (rest) ->
+    let angle = ((Float.of_int i) *. segment_angle) +. a in
+    let next : Primitives.point = {
+      x = Int.of_float (fr *. sin angle) ;
+      y = Int.of_float (fr *. cos angle) ;
+    } in
+    match i with
+    | 0 -> next :: rest
+    | _ -> next :: loop (i - 1) rest
+  ) in 
+  let points = loop (sides - 1) [] in
+  let shifted = List.map (fun (p : Primitives.point) : Primitives.point -> 
+    {
+      x = p.x + x ;
+      y = p.y + y ;
+    }
+  ) points in
+  Primitives.Polygon (shifted, col)
+  
+(* ----- *)
+
+let boot s =
+  Framebuffer.init (Screen.dimensions s) (fun _x _y -> 0) 
+  
+let tick t s _prev = 
+  let w, h = Screen.dimensions s in
+  (* Fade what came before *)
+  (* let buffer = Framebuffer.shader (fun pixel -> 
+    match pixel with
+    | 0 -> pixel
+    | _ -> (pixel - 1)
+  ) prev in *)
+  let buffer = Framebuffer.init (Screen.dimensions s) (fun _x _y -> 0)  in
+
+  let hex = drawpoly ((t / 1) mod w) (h/2) 60 (((t / 150) mod 10) + 3) ((Float.of_int t) /. -50.) (t mod 150) in
+  Framebuffer.render buffer [hex];
+
+  buffer
+
+(* ----- *)
+
+let () =
+  let pal = List.rev (Palette.to_list (Palette.generate_mono_palette 150)) in
+  let screen = Screen.create 640 480 1 (Palette.of_list pal) in
+  Tcc.run screen "Genuary Day 8: Chaotic" (Some boot) tick
