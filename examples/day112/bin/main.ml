25ef73e3955524fbcd1e87bc91880227f63fb16a
Michael Dales <michael@digitalflapjack.com>
2024-01-13 10:16:22 +0000

    Something for day 11

--- examples/day112/bin/main.ml
+++ examples/day112/bin/main.ml
@@ -6,12 +6,12 @@ let band_height = 20
 
 (* ----- *)
 
-let generate_tile _t w h base_col over_col : Primitives.t list =
+let generate_tile ft w h base_col over_col : Primitives.t list =
   let rec loop index rest = (
     let this = Primitives.FilledRect (
       {x = 0 ; y = index * band_height},
       {x = w ; y = (index + 1) * band_height},
-      if ((Random.int 5) > 0) then over_col else base_col) in
+      if ((sin ft) *. (sin ((Float.of_int index) /. (Random.float 10.))) > 0.5) then over_col else base_col) in
     match index with
     | 0 -> this :: rest
     | _ -> this :: loop (index - 1) rest
@@ -32,7 +32,8 @@ let tick t s _p =
     for ix = 0 to (cols - 1) do
       let x = ix * xstep
       and y = iy * ystep in
-      let tile = generate_tile t (w/cols) (h/rows) (iy) ix in
+      Random.init (x + y);
+      let tile = generate_tile ((Float.of_int t) /. 100.) (w/cols) (h/rows) (iy) ix in
       let shifted = List.map (fun (p : Primitives.t) : Primitives.t ->
         match p with
         | Primitives.FilledRect (p1, p2, col) -> Primitives.FilledRect ({x=p1.x + x; y = p1.y + y}, {x=p2.x + x ; y = p2.y + y}, col)
