be0c07b812f0ac6c9670e2a67a35955b86a8b58b
Michael Dales <michael@digitalflapjack.com>
2024-01-14 09:44:51 +0000

    Day 12 fiddling

--- examples/day12/bin/main.ml
+++ examples/day12/bin/main.ml
@@ -5,41 +5,14 @@ let tick t s _p =
   let ft = (Float.of_int t) /. 20. in
   Framebuffer.init (Screen.dimensions s) (fun x y ->
     let fx = Float.of_int x and fy = Float.of_int y in
-    let c = ((sin ((sin (ft /. 23.)) +. fx /. 150.) *. 1.) +. (atan(((fy +. fx) /. 70.) -. ft)) *. 1.) in
-    (* let c = 0. in *)
-    let d = ((sin ((sin (ft /. 23.)) +. fy /. 250.) *. 1.) +. (sin((fx /. 150.) -. ft)) *. 1.) in
-    (* let d = 0. in *)
-    let e = ((cos ((sin (fx /. 103.)) +. ft /. 250.) *. 1.) *. (sin((ft /. 10.) -. 0.)) *. 3.) in 
-    (* let ic = Int.of_float (c +. d +. e) in *)
-    (* let mc = ic mod 16 in  *)
-    (* if ic < 0 then 0 else ic *)
+    let c = ((sin ((sin (ft /. 23.)) +. fx /. 150.) *. 1.) +. (atan(((fy +. fx) /. 70.) -. ft)) *. 1.)
+    and d = ((sin ((sin (ft /. 23.)) +. fy /. 250.) *. 1.) +. (sin((fx /. 150.) -. ft)) *. 1.)
+    and e = ((cos ((sin (fx /. 103.)) +. ft /. 250.) *. 1.) *. (sin((ft /. 10.) -. 0.)) *. 3.) in 
     let col = c +. d +. e in
     let r = Float.rem (col) 1. in
-    let m = if r > (Random.float 1.) then 1  else 0 in
+    let m = if r > (Random.float 1.) then 1 else 0 in
     (Int.of_float col) + m 
-    (* if (c > d) then 2 else 1 *)
   ) 
-  (* |>
-  Framebuffer.shaderi (fun x y buffer ->
-    let pixels = [
-      (-1, -1) ;
-      (0, -1) ;
-      (1, -1) ;
-      (-1, 0) ;
-      (0, 0) ;
-      (1, 0) ;
-      (-1, 1) ;
-      (0, 1) ;
-      (1, 1) ;
-    ] in
-    let vals = List.filter_map (fun p -> 
-      let a, b = p in
-      Framebuffer.pixel_read (x + a) (y + b) buffer
-    ) pixels in
-    let fvals = List.map Float.of_int vals in
-    let avg = (List.fold_left Float.add 0. fvals) /. (Float.of_int (List.length fvals)) in
-    Int.of_float avg
-  ) *)
 
 let () = 
   Palette.generate_plasma_palette 16 |>
