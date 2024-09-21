3b8f1025c06ca935cff551e69beafcc944121254
Michael Dales <mwd24@cam.ac.uk>
2024-09-21 14:22:10 +0100

    Documenation for framebuffer

--- src/framebuffer.mli
+++ src/framebuffer.mli
@@ -1,8 +1,7 @@
 
 (** Provides the simulated framebuffer for Claudius.
 
-The framebuffer is logically an array of memory in which you draw
-using palette entries. *)
+The framebuffer is logically an array of memory in which you draw using palette entries, similar to say how VGA worked on old PCs. *)
 
 type t
 
@@ -14,23 +13,30 @@ val init: int * int -> (int -> int -> int) -> t
     coordinates of the pixel and should return the colour there. *)
 
 
-(** {1 Single pixel operations} *)
-
-val pixel_write: int -> int -> int -> t -> unit
-(** [pixel_write x y colour framebuffer] Set the pixel at the specified coordinate to palette colour
-    in the provided framebuffer. If the coordinate is outside the framebuffer then nothing is drawn, but
-    there is no error. *)
-
-val pixel_read: int -> int -> t -> int option
-(** [pixel_read x y framebuffer] Get the pixel colour at the specified coordinate in the
-    provided framebuffer. If the coordinate is outside the framebuffer you get None,
-    otherwise Some colour. *)
-
 
 (** {1 Drawing operations}
     These operations let you modify the framebuffer with basic shapes. This list isn't exhaustive,
     but just some basics to let people get up and running. Shapes will be automatically clipped to fit
     the framebuffer. Position (0, 0) is in the top left of the screen. *)
+
+(** {2 Functional style }
+  In functional style, you should aim to build up a transformation pipeline where you effectively have a series of steps like so:
+
+  + Generate data
+  + Advance data by time step t
+  + Transform data to list of rendering Primitive.t elements
+  + Pass list to Frambuffer.render
+*)
+
+
+val render: t -> Primitives.t list -> unit
+(** [render framebuffer primitives] Takes a list of primative shapes and calls the appropriate
+    drawing operation for each in turn to render them into the provide framebuffer. *)
+
+
+(** {2 Imperative style }
+  As an alternative to the functional style above, you can just draw directly to the framebuffer in an imperative style, one operation at a time.
+*)
 
 val draw_line: int -> int -> int -> int -> int -> t -> unit
 (** [draw_line x0 y0 x1 y1 colour framebuffer] Draws a line between ([x0], [y0]) ([x1], [y1]) specified in
@@ -68,52 +74,81 @@ val filled_polygon: (int * int) list -> int -> t -> unit
 (** [filled_polygon points colour framebuffer] Draws a filled polygon made from the list of [points]
     in the specified [colour] into [framebuffer]. *)
 
-(** {1 Text rendering operations} *)
-
 val draw_char: int -> int -> Font.t -> char -> int -> t -> int
-(** [draw_char x y font c colour framebuffer] Draws a single character [c] in the specified [colour] using [font]. The top left of 
+(** [draw_char x y font c colour framebuffer] Draws a single character [c] in the specified [colour] using [font]. The top left of
     the charcter is the point specified by position ([x], [y]). The return value is the number of pixels wide the character was. *)
 
 val draw_string: int -> int -> Font.t -> string -> int -> t -> int
-(** [draw_string x y font s colour framebuffer] Draws the string [s] in the specified [colour] using [font]. The top left of 
+(** [draw_string x y font s colour framebuffer] Draws the string [s] in the specified [colour] using [font]. The top left of
     the first charcter is the point specified by position ([x], [y]). The return value is the number of pixels wide the string was. *)
 
-(** {1 Framebuffer wide operations} *)
+
+(** {1 Pixel operations}
+  Rather than working with shape primatives, sometimes you want to work with pixels directly, for example creating plasma effects
+  or drawing fractals, etc. Again, Claudius has both a functional approach for this and an imperative approach.
+*)
+
+(** {2 Functional style}
+  In the functional style you can pass functions that will be called per pixel on the source, doing a full transform of the framebuffer to a
+  new state. Some common tricks that emerge from this are examples like clearing the screen:
+
+  {[
+  Framebuffer.map_inplace (fun _ -> 0) existing_framebuffer;
+  ]}
+
+  Or to fade out the previous frame (assuming you have a palette where lower values are darker):
+
+  {[
+  Framebuffer.map_inplace (fun x -> match pixel with 0 -> 0 | x -> x - 1) existing_framebuffer;
+  ]}
+*)
 
 type shader_func = int -> int
 
 type shaderi_func = int -> int -> t -> int
 
 val shader: shader_func -> t -> t
-(** [shader f framebuffer] Generates a new framebuffer of the same dimensions by applying the provided 
+(** [shader f framebuffer] Generates a new framebuffer of the same dimensions by applying the provided
     function [f] to each pixel value in the original to generate a new pixel in the target. *)
 
 val shaderi: shaderi_func -> t -> t
-(** [shader f framebuffer] Generates a new framebuffer of the same dimensions by applying the provided 
+(** [shader f framebuffer] Generates a new framebuffer of the same dimensions by applying the provided
     function [f] to each pixel value and its coordinates in the original to generate a new pixel in the target. *)
 
 val shader_inplace: shader_func -> t -> unit
-(** [shader f framebuffer] Updates a framebuffer by applying the provided 
+(** [shader f framebuffer] Updates a framebuffer by applying the provided
     function [f] to each pixel value to update its value. *)
 
 val shaderi_inplace: shaderi_func -> t -> unit
-(** [shader f framebuffer] Updates a framebuffer by applying the provided 
+(** [shader f framebuffer] Updates a framebuffer by applying the provided
     function [f] to each pixel value and its coordinate value to update its value. *)
 
 val merge: (int -> int -> int) -> t -> t -> t
-(** [merge f first second] Takes two framebuffers of equal size and applys the function [f] to each pixel 
+(** [merge f first second] Takes two framebuffers of equal size and applys the function [f] to each pixel
     pair in turn to generate a new framebuffer. *)
 
 val merge_inplace: (int -> int -> int) -> t -> t -> unit
-(** [merge_inplase f first second] Takes two framebuffers of equal size and applys the function [f] to each pixel 
+(** [merge_inplase f first second] Takes two framebuffers of equal size and applys the function [f] to each pixel
     pair in storing the result back in the first provided framebuffer. *)
 
 
-(** {1 Misc} *)
 
-val render: t -> Primitives.t list -> unit
-(** [render framebuffer primitives] Takes a list of primative shapes and calls the appropriate
-    drawing operation for each in turn to render them into the provide framebuffer. *)
+(** {2 Imperative style}
+  Sometimes you don't want to transform the entire framebuffer, and so you can do per pixel calls also.
+*)
+
+val pixel_write: int -> int -> int -> t -> unit
+(** [pixel_write x y colour framebuffer] Set the pixel at the specified coordinate to palette colour
+    in the provided framebuffer. If the coordinate is outside the framebuffer then nothing is drawn, but
+    there is no error. *)
+
+val pixel_read: int -> int -> t -> int option
+(** [pixel_read x y framebuffer] Get the pixel colour at the specified coordinate in the
+    provided framebuffer. If the coordinate is outside the framebuffer you get None,
+    otherwise Some colour. *)
+
+
+(** {1 Misc} *)
 
 val to_array: t -> int array array
 (** [to_array framebuffer] converts the framebuffer into a 2D array. The top level array is an array
